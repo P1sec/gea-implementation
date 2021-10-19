c04bdaf8271b0480747b07e89ffeb572669cb0ab
p1-bmu <benoit.michau@p1sec.com>
2021-10-19 16:54:09 +0200

    c: align header with the licensing

--- gea-c/gea12_test.c
+++ gea-c/gea12_test.c
@@ -1,16 +1,36 @@
 /*----------------------------------------------------------------------------*/
-/*
+/* Software Name : gea12
+ * Version : 0.1
+ *
+ * Copyright 2021. Benoit Michau. P1Sec.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------
+ * File Name : gea12_test.c
+ * Created : 2021-10-19
+ * Authors : Benoit Michau 
+ *--------------------------------------------------------
+  
   Implementation of the GPRS encryption algorithms GEA1 and GEA2
   From the research paper:
   https://eprint.iacr.org/2021/819.pdf
   
-  
-  Copyright P1 Security, 2021/28/09
-  
   This implementation has been developped for a 64 bits little-endian system
-  complying with the C99 standard
-*/
+  complying with the C99 standard                                             */
 /*----------------------------------------------------------------------------*/
+
 
 #include "gea12.h"
 #include <stdio.h>
