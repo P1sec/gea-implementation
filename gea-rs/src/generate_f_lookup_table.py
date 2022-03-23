879592a06b7efeaf0ffb9ea691a5eca7e6367cda
p1-bmu <benoit.michau@p1sec.com>
2022-03-23 11:55:01 +0100

    add licence headers in all rust files

--- gea-rs/src/generate_f_lookup_table.py
+++ gea-rs/src/generate_f_lookup_table.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python3
 #-*- encoding: Utf-8 -*-
 
+#/**
+# * Software Name : gea12
+# * Version : 0.1
+# *
+# * Copyright 2021. Marin Moulinier. P1Sec.
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU Affero General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU Affero General Public License for more details.
+# *
+# * You should have received a copy of the GNU Affero General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *
+# *--------------------------------------------------------
+# * File Name : generate_f_lookup_table.py
+# * Created : 2021-10-19
+# * Authors : Marin Moulinier
+# *--------------------------------------------------------
+#*/
+
+
 """
     This script was used to generate the original contents
     of the "f_lookup_table.rs" file
--- gea-rs/src/registers/a_register.rs
+++ gea-rs/src/registers/a_register.rs
@@ -1,3 +1,34 @@
+//-----------------------------------------------------------------------------/
+// Software Name : gea12
+// Version : 0.1
+//
+// Copyright 2021. Marin Moulinier. P1Sec.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------
+// File Name : registers/a_register.rs
+// Created : 2021-10-19
+// Authors : Marin Moulinier
+//--------------------------------------------------------
+//  
+//  Implementation of the GPRS encryption algorithms GEA1 and GEA2
+//  From the research paper:
+//  https://eprint.iacr.org/2021/819.pdf
+//-----------------------------------------------------------------------------/
+
+
 use crate::lfsr::LinearFeedbackShiftRegister;
 use crate::f_lookup_table::F_LOOKUP_TABLE;
 
